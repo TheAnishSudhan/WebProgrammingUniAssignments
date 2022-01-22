bfc840a270f2b38e9e87308f2b6945e5ff528de5
Anish Sudhan <64979922+TheAnishSudhan@users.noreply.github.com>
2022-01-22 22:57:43 +0530

    Lab 6 labwork + practical

---
+++ Lab6/js1_pract.js
@@ -0,0 +1,19 @@
+var i, count=0;
+
+for(i=0;i<5;i++)
+{
+  count=count+1;
+}
+document.write(count);
+//
+// var i, count = 0;
+// document.write("\n")
+//
+// document.write("Restarting loop count");
+//
+// for (i = 0; i < 5; i++) {
+//   count=count+1;
+// }
+// document.write("\n")
+// document.write(count);
+// document.write("\n");
